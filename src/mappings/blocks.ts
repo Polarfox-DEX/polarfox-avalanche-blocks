d5f08dd53c5078cbd6e37cfcad18525d8ff1fb82
Clément Aguilar <aguilar.clement@outlook.fr>
2021-06-27 16:15:07 +0200

    Switch from Ethereum to Fuji

--- src/mappings/blocks.ts
+++ src/mappings/blocks.ts
@@ -1,12 +1,12 @@
 import {
-    EthereumBlock
+    EthereumBlock as AvalancheBlock
 } from "@graphprotocol/graph-ts"
 
 import {
     Block
 } from "../../generated/schema"
 
-export function handleBlock(block: EthereumBlock): void {
+export function handleBlock(block: AvalancheBlock): void {
     let id = block.hash.toHex()
     let blockEntity = new Block(id);
     blockEntity.number = block.number;
